cbf375f2dbe2761c9b0729c9cc54b57a350982ff
guilhermehub12 <guilhermee1312@hotmail.com>
2023-01-20 22:07:29 -0300

    build: import global css and habits cpnts

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,14 @@
+import './styles/global.css'
+
+import { Habit } from "./components/Habits"
+
 function App() {
     return (
-    <h1>Hello World</h1>
+    <>
+      <Habit completed={2} />
+      <Habit completed={5} />
+      <Habit completed={10} />
+      </>
   )
 }
 
